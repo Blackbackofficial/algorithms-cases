119113f0539acebd8426bf4bd8b47ca2a5cc0b9e
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-02-04 23:16:15 +0300

    Add start prj

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    printf("Мне нравится язык %c %s", 'C', "и к тому же очень сильно!");
-    return 0;
-}
---
+++ sort/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Иван Чернов on 04.02.2021.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Worl4g4tg4grg464g4g6d!" << std::endl;
+    return 0;
+}
